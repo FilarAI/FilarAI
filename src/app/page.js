84e4882a06e2067ae508b1b763f2edaaea5cc17d
Kacper Filar <kacperfilarkontakt@gmail.com>
2026-03-11 08:44:14 +0100

    feat: simplified homepage to hero and cta sections

--- src/app/page.js
+++ src/app/page.js
@@ -1,45 +1,13 @@
 import { FloatingHeader } from '@/components/sections/FloatingHeader';
 import { Hero } from '@/components/sections/Hero';
-import { Challenge } from '@/components/sections/Challenge';
-import { Fomo } from '@/components/sections/Fomo';
-import { Services } from '@/components/sections/Services';
-import { Portfolio } from '@/components/sections/Portfolio';
-import { Clients } from '@/components/sections/Clients';
-import { Testimonials } from '@/components/sections/Testimonials';
-import { Process } from '@/components/sections/Process';
-import { About } from '@/components/sections/About';
 import { FinalCTA } from '@/components/sections/FinalCTA';
-import { Benefits } from '@/components/sections/Benefits';
-import { Problem } from '@/components/sections/Problem';
-import { ClientLogos } from '@/components/sections/ClientLogos';
-import { Solution } from '@/components/sections/Solution';
-import { HowItWorks } from '@/components/sections/HowItWorks';
-import { Expertise } from '@/components/sections/Expertise';
-import { PortfolioPreview } from '@/components/sections/PortfolioPreview';
-import { Footer } from '@/components/sections/Footer';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white dark:bg-dark-bg text-text-primary dark:text-white">
       <FloatingHeader />
       <Hero />
-      <Challenge />
-      <Fomo />
-      <Services />
-      <Portfolio />
-      <Clients />
-      <Testimonials />
-      <Process />
-      <About />
       <FinalCTA />
-      <ClientLogos />
-      <Benefits />
-      <Problem />
-      <Solution />
-      <HowItWorks />
-      <Expertise />
-      <PortfolioPreview />
-      <Footer />
     </main>
   );
 }
